72e558ea3f5450eb3589030a85e4a2aaa13090d4
jasmithakorlapati <113580828+jasmithakorlapati@users.noreply.github.com>
2024-10-17 14:49:45 +0530

    Add project files.

---
+++ ExampleOfM1/CapgException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleOfM1
+{
+    public class CapgException : Exception
+    {
+        public CapgException(string msg): base(msg)
+        {
+            
+        }
+    }
+}
---
+++ ExampleOfM1/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace ExampleOfM1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 100;
+            int d = 0;
+            int ch = 1;
+
+            while (ch != -1)
+            {
+                Console.WriteLine("Enter your choice (-1) Exit else Contine to Divide numbers");
+                ch = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    Console.WriteLine("Enter divider value");
+                    d = int.Parse(Console.ReadLine());
+                    int result = Div(n, d);
+                    Console.WriteLine(result);
+
+                }
+                catch (CapgException ex) {
+                    Console.WriteLine(ex.Message);
+                }
+                catch ( DivideByZeroException ex)
+                {
+                    Console.WriteLine("Please enter valid input 1-100");
+                    continue;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Some Internal Error");
+                }
+
+            }
+
+
+
+
+            Console.ReadLine();
+        }
+
+        private static int Div(int n, int d)
+        {
+            int ans = 0;
+            try
+            {
+                ans = n / d;
+
+            }
+            catch (Exception ex)
+            {
+
+                throw new CapgException("Internal error");
+            }
+
+
+            return n / d;
+        }
+    }
+}
